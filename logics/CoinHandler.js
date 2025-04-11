9afc64255b26b9e8fdf371f915f292c086e351b3
Alihan Gündoğdu <alihangundogdu01@gmail.com>
2025-04-11 11:28:00 +0300

    Add Coin and CoinHandler classes for coin management

---
+++ logics/CoinHandler.js
@@ -0,0 +1,9 @@
+class CoinHandler extends Handler{
+    /**
+     * @param {Grid} grid
+     * @param {Player} player
+     */
+    constructor(grid,player){
+        super(grid,player);
+    }
+} 
---
+++ models/BattleModels/Coin.js
@@ -0,0 +1,12 @@
+class Coin extends Entity{
+    value = 1;
+
+    /**
+     * 
+     * @param {DrawAttributes} drawAttributes 
+     * @param {MotionAttributes} motionAttributes 
+     */
+    constructor(drawAttributes,motionAttributes){
+        super(drawAttributes,motionAttributes);
+    }
+}
